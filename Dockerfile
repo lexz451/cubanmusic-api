dad3e44fef1ec2c42f5f2bf9d177b05365e87806
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2021-09-24 16:03:17 -0400

    Remove docker support

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:16-alpine
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-COPY .env .env
-ENTRYPOINT ["java", "-jar", "/app.jar", "-Denv=.env"]
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-docker-compose down --rmi all
+#!/bin/bash
 mvn clean package -DskipTests=true
-docker-compose up -d
+nohup java -jar ./target/cubanmusic-api-0.0.1-SNAPSHOT.jar > ./log.txt 2>&1 &
+echo $! > ./pid.file
