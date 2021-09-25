2b83f34c3f9a33e782826825a03fc5b492bb8726
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2021-09-24 23:56:53 -0400

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package -DskipTests=true
-nohup java -jar ./target/cubanmusic-api-0.0.1-SNAPSHOT.jar > ./log.txt 2>&1 &
+nohup mvn spring-boot:run > ./log.txt 2>&1 &
 echo $! > ./pid.file
