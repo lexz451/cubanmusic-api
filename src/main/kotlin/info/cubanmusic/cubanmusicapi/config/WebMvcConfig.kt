6c842ce5eabb44e237ea2f6ec227db365749bd75
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2021-10-19 15:34:37 -0400

    Update WebMvcConfig.kt

--- src/main/kotlin/info/cubanmusic/cubanmusicapi/config/WebMvcConfig.kt
+++ src/main/kotlin/info/cubanmusic/cubanmusicapi/config/WebMvcConfig.kt
@@ -14,6 +14,7 @@ class WebMvcConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
+            .allowedHeaders("*")
             .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
             .maxAge(MAX_AGE_SECS.toLong())
     }
