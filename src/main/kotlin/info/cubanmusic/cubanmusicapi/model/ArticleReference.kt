084e444cee232a84f247e7a9cfe98137e2870ed5
Alejandro R. Zaldivar <lexz.zaldivar@gmail.com>
2021-10-21 12:41:51 -0400

    Update

--- src/main/kotlin/info/cubanmusic/cubanmusicapi/model/ArticleReference.kt
+++ src/main/kotlin/info/cubanmusic/cubanmusicapi/model/ArticleReference.kt
@@ -5,13 +5,19 @@ import org.springframework.data.jpa.domain.AbstractAuditable
 import java.util.*
 import javax.persistence.*
 
+@Table(name = "ArticleReference", indexes = [
+    Index(name = "idx_articlereference_id_unq", columnList = "id, title", unique = true)
+])
 @Entity
-open class ArticleReference : BibliographicReference() {
+open class ArticleReference : AbstractAuditable<User,Long>() {
     open var title: String? = null
     open var url: String? = null
-
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "artist_id")
     open var artist: Artist? = null
+    open var source: String? = null
+    open var author: String? = null
+    @Temporal(TemporalType.DATE)
+    open var date: Date? = null
 }
--- src/main/kotlin/info/cubanmusic/cubanmusicapi/model/QuoteReference.kt
+++ src/main/kotlin/info/cubanmusic/cubanmusicapi/model/QuoteReference.kt
@@ -6,12 +6,15 @@ import java.util.*
 import javax.persistence.*
 
 @Entity
-open class QuoteReference : BibliographicReference() {
+open class QuoteReference : AbstractAuditable<User, Long>() {
     @Lob
     open var quote: String? = null
-
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "artist_id")
     open var artist: Artist? = null
+    open var source: String? = null
+    open var author: String? = null
+    @Temporal(TemporalType.DATE)
+    open var date: Date? = null
 }
